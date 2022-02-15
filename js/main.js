ce0410ea59b2926666328ea1d3204875d50fd0e9
Manoj Kumar <manoj@facetinteractive.com>
2022-02-15 18:19:08 +0530

    CSS modification

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-function navMenu() {
-    document.getElementById('nav').classList.toggle('nav');
- }
+function openBtn() {
+    document.getElementById('nav').style.width = '280px';
+}
+function closeBtn(){
+    document.getElementById('nav').style.width = '0px';
+}
+
